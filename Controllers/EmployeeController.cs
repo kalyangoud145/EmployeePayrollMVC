32b41983b671d9c3867391d416fe87ed3eda4d3e
kalyangoud145 <kalyangoud145@gmail.com>
2020-12-20 11:48:39 +0530

    [Kalyan] added: MVC structure files and classes

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using EmployeePayrollMVC.Models;
+using EmployeePayrollMVC.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeePayrollMVC.Controllers
+{
+    [Authorize]
+    public class EmployeeController : Controller
+    {
+        public ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmployeePayrollMVC.Startup))]
+namespace EmployeePayrollMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
