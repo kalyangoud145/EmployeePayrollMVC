8b879f741c2ef5ee03200c202890d512eea07a04
kalyangoud145 <kalyangoud145@gmail.com>
2020-12-20 12:46:13 +0530

    [Kalyan] added: method to edit employee details

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -108,5 +108,74 @@ namespace EmployeePayrollMVC.Controllers
                 throw e;
             }
         }
+        /// <summary>
+        /// retrive the data from database and display in the edit webpage
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public ActionResult Edit(EmployeeViewModel item)
+        {
+            RegisterEmpRequestModel emp = new RegisterEmpRequestModel
+            {
+                EmpId = item.EmpId,
+                Name = item.Name,
+                Gender = item.Gender,
+                Department = item.Department,
+                SalaryId = item.SalaryId.ToString(),
+                StartDate = item.StartDate,
+                Description = item.Description
+            };
+
+            return View(emp);
+        }
+        /// <summary>
+        /// Edit the existing employee details and update the data in database
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns></returns>
+        public ActionResult EditEmployee(RegisterEmpRequestModel employee)
+        {
+            bool result = EditEmployeeService(employee);
+            if (result == true)
+            {
+                List<EmployeeViewModel> list = GetAllEmployee();
+                return View("EmployeeList", list);
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+        public bool EditEmployeeService(RegisterEmpRequestModel employee)
+        {
+            try
+            {
+                int departmentId = db.Departments.Where(x => x.DeptName == employee.Department).Select(x => x.DeptId).FirstOrDefault();
+
+                Employee emp = db.Employees.Find(employee.EmpId);
+                emp.Name = employee.Name;
+                emp.SalaryId = Convert.ToInt32(employee.SalaryId);
+                emp.StartDate = employee.StartDate;
+                emp.Description = employee.Description;
+                emp.Gender = employee.Gender;
+                emp.DepartmentId = departmentId;
+
+                //db.Entry(emp).State = EntityState.Modified;
+
+                int result = db.SaveChanges();
+                if (result > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
     }
 }
