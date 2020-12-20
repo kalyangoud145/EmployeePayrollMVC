5af9e920c2dfddab8d37dff4601ca22e6dc37785
kalyangoud145 <kalyangoud145@gmail.com>
2020-12-20 12:19:39 +0530

    [Kalyan] added: method to retrive data using id

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -17,5 +17,41 @@ namespace EmployeePayrollMVC.Controllers
         {
             return View();
         }
+        /// <summary>
+        /// Retrive data from the data base
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult EmployeeList()
+        {
+            List<EmployeeViewModel> list = GetAllEmployee();
+            return View(list);
+        }
+        public List<EmployeeViewModel> GetAllEmployee()
+        {
+            try
+            {
+                List<EmployeeViewModel> list = (from e in db.Employees
+                                                join d in db.Departments on e.DepartmentId equals d.DeptId
+                                                join s in db.Salaries on e.SalaryId equals s.SalaryId
+                                                select new EmployeeViewModel
+                                                {
+                                                    EmpId = e.EmpId,
+                                                    Name = e.Name,
+                                                    Gender = e.Gender,
+                                                    DepartmentId = d.DeptId,
+                                                    Department = d.DeptName,
+                                                    SalaryId = s.SalaryId,
+                                                    Amount = s.Amount,
+                                                    StartDate = e.StartDate,
+                                                    Description = e.Description
+                                                }).ToList<EmployeeViewModel>();
+
+                return list;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
     }
 }
